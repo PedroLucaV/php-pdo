ca65039daf22d0caacb611304ccf6d7d4af45e42
Pedro Lucas <izielucas2016@gmail.com>
2025-06-09 12:11:06 -0300

    feat: created project

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+    $dsn = 'mysql:host=localhost;dbname=php_com_pdo';
+    $user = 'root';
+    $password = '';
+
+    try{
+        $conexao = new PDO($dsn, $user, $password);
+    }catch(PDOException $e){
+        echo "Error [{$e->getCode()}] -> {$e->getMessage()}";
+    }
